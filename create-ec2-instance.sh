817dc05f14fb80cb5eb3ce9f9869b000e04d8c0b
padhiarigithub <111277991+padhiarigithub@users.noreply.github.com>
2023-03-09 14:16:05 +0530

    Update create-ec2-instance.sh

--- create-ec2-instance.sh
+++ create-ec2-instance.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 # Set variables for EC2 instance configuration
-ami_id="ami-082b1f4237bd816a1"    # Amazon Linux 2 AMI ID (replace with your desired AMI ID)
-instance_type="t2.micro"          # Instance type (replace with your desired instance type)
-security_group_id="sg-017f5ac79caac7c6b"  # Security group ID (replace with your desired security group ID)
-key_pair_name="freqtrade"        # Key pair name (replace with your desired key pair name)
-region="ap-southeast-1"                # Region (replace with your desired region)
+AMI_ID="ami-082b1f4237bd816a1" # replace with desired AMI ID
+INSTANCE_TYPE="t2.micro"
+SECURITY_GROUP_ID="sg-017f5ac79caac7c6b" # replace with desired security group ID
+SUBNET_ID="subnet-0bb79b484d5a54899" # replace with desired subnet ID
+KEY_NAME="freqtrade" # replace with desired key pair name
+# Create instance
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID --key-name $KEY_NAME --output text --query 'Instances[*].InstanceId')
 
-# Create EC2 instance
-instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type $instance_type --security-group-ids $security_group_id --key-name $key_pair_name --region $region | jq -r '.Instances[].InstanceId')
+# Wait for instance to be running
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-echo "EC2 instance created with ID: $instance_id"
-
-# Wait for EC2 instance to become available
-aws ec2 wait instance-status-ok --instance-ids $instance_id --region $region
-
-# Sleep for 2 minutes before terminating instance
-echo "Waiting for 2 minutes before terminating EC2 instance..."
+# Sleep for 2 minutes
 sleep 120
 
-# Terminate EC2 instance
-aws ec2 terminate-instances --instance-ids $instance_id --region $region
+# Terminate instance
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID
 
-echo "EC2 instance terminated with ID: $instance_id"
+echo "EC2 instance terminated with ID: $INSTANCE_ID"
+
